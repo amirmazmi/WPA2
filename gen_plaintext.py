c23d9838ecedd4e6bec14a61521c95ba26283ba9
amirmazmi <amirmazmi@users.noreply.github.com>
2019-09-10 14:42:25 +0800

    Add files via upload

--- gen_plaintext.py
+++ gen_plaintext.py
@@ -8,52 +8,68 @@
 import os
 import itertools
 import sys
+import datetime as dt
 from time import sleep
 
 #generate plaintext 
 #small subset since too much time to calculate hash
-charlist = """ABCDE"""
-charlen = 9
-combitn = len(charlist)**charlen
-#formula is length ^characters
+charlist = """ABCDEFGHIJK"""
+charlen = 7
+combitn = len(charlist)**charlen       #formula is length ^characters
+
+sleepdur = 1
 
 pathfile = os.getcwd()
-namefile = 'RUNNING ' + str(charlen) + '-char-plaintext.txt'
+namefile = 'RUNNING ' + str(charlen+1) + '-char-plaintext.txt'
 outfile = os.path.join(pathfile,namefile)
 
 # entire printable ASCII 
 # !#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`"abcdefghijklmnopqrstuvwxyz{|}~
 
-sys.stdout.write('\n\033[33m SERIAL: Running... \n\n\033[0m')
+sys.stdout.write('\n\033[33m SERIAL: Running... \n\033[0m')
 
 li=[]
 
 try:
+	print("\t Generating ", format(combitn,',d'), "combinations...\n")
+	startTime = dt.datetime.now()
 	with open(outfile,'w') as f:
 		#permutation 
 		for k,i in enumerate(itertools.product(charlist,repeat=charlen)):
-			li.append(''.join(i))
+			li.append(''.join( ("A",)+i) )
 		
-			if (k%200==0):
+			if (k%500==0):
 				f.write('\n'.join(li) )
 				f.write('\n')	
 				li=[]
 
-#			add sleep for every million
-			if (k !=0 and k%1000000==0):
-				sleep(3)
-				print("Sleep for 3 seconds:","[%d%%]" % (k/combitn *100), format(k,',d'),"of",format(combitn,',d') )
+#			add sleep for every 10 million
+			if (k !=0 and k%10000000==0):
+				sleep(sleepdur)
+				print("Sleep for",sleepdur, "seconds:","[%d%%]" % (k/combitn *100), format(k,',d'),"of",format(combitn,',d') )
 		
 		f.write('\n'.join(li) )
+		f.write('\n')
 		print("\n Generated",format(k+1, ',d'), "phrases")
-		
+	
+	f.close()
 	os.rename(namefile,namefile[8:])			
-	print(" \n Done")
+
+	timeTaken = (dt.datetime.now() - startTime)
+	minDuration = timeTaken/dt.timedelta(seconds=60)
+
+	print("     Total time taken", 
+					" \n\t minutes:", format(minDuration,  '.3f'), 
+					" \n\t seconds:", format( timeTaken.total_seconds(), '.3f' ),
+					"\n\t rate   :", format(  k/(timeTaken.total_seconds()*10**6), '.2f' ), "million phrases per second"	)
+
+
+	sys.stdout.write('\n\n\033[33m ### DONE ### \n\n\n\033[0m')
+
 
 except KeyboardInterrupt:
 	sys.stdout.write('\033[33m') #terminal colour
 	sys.stdout.write('\n User Interrupt \n')
 	sys.stdout.write('\033[0m')	#reset color	
 	print("Killed at", format(k,',d') , li[-1] )
-
 
--- mp-gen_plaintext.py
+++ mp-gen_plaintext.py
@@ -13,8 +13,13 @@ import multiprocessing as mp
 from time import sleep
 import queue
 
+
+charlen = 7
+charlist = """ABCDEFGHIJK"""
+combitn = len(charlist)**charlen       #formula is length ^characters
+
 pathfile = os.getcwd()
-namefile = 'mp-plaintext'
+namefile = 'RUNNING ' + str(charlen+1) + '-char-plaintext.txt'
 outfile = os.path.join(pathfile,namefile)
 
 #generate plaintext - small subset since too much time to calculate hash
@@ -24,44 +29,61 @@ outfile = os.path.join(pathfile,namefile)
 #	!#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~
 
 def gen_text(comb):
-	ptext = (''.join(comb))
+	ptext = (''.join( ("A",)+comb) )
 	q.put(ptext)
 #	print(ptext, os.getpid())
 	return 0
 
 if __name__=='__main__':
-	charlist = """ABC"""
+	
 	sys.stdout.write('\n\033[33m PARALLEL: Running... \n\n\033[0m')
 	sys.stdout.flush()
 
 	try:
+		print("\t Generating ", format(combitn,',d'), "combinations...\n")
+		startTime = dt.datetime.now()
 		q=mp.Queue()		
 		with open(outfile,'w') as f:
 
 			#permutation 
 #			perm = itertools.product(charlist,repeat=8)
 			with mp.Pool(mp.cpu_count()) as pool:
-				result = pool.imap( gen_text, (itertools.product(charlist,repeat=8)))
+				result = pool.imap( gen_text, (itertools.product(charlist,repeat=charlen)))
 				k=0
 				que=[]
 				while (1):
 					try:
 						que.append(q.get(timeout=0.1))
 						
-						if (k%10==0 and k!=0):
+						if (k%500==0 and k!=0):
+							print("\t Length should be 500:", length(que) )
 							f.write('\n'.join(que)) 
 							f.write('\n')
 							que=[]
 #						print(k,que)
 						k+=1
+						
 					except queue.Empty:
 						f.write('\n'.join(que)) 
-						print("\n\n", k,"exit while loop")
+						f.write('\n')
+						print("\n\n\t Exiting while loop")
 						break
-				q.close()
-			print("\n Generated",format(k, ',d'), "phrases")
 				
-		print(" \n Done")
+				q.close()
+		f.close()
+		os.rename(namefile,namefile[8:])			
+			
+		print("\n\t Generated",format(k, ',d'), "phrases")
+	
+		timeTaken = (dt.datetime.now() - startTime)
+		minDuration = timeTaken/dt.timedelta(seconds=60)
+		print("     Total time taken", 
+				" \n\t minutes:", format(minDuration,  '.3f'), 
+				" \n\t seconds:", format( timeTaken.total_seconds(), '.3f' ),
+				"\n\t rate   :", format(  k/(timeTaken.total_seconds()*10**6), '.2f' ), "million phrases per second"	)
+
+
+	sys.stdout.write('\n\n\033[33m ### DONE ### \n\n\n\033[0m')
 
 	except KeyboardInterrupt:
 		q.close()
@@ -69,4 +91,6 @@ if __name__=='__main__':
 		sys.stdout.write('\n User Interrupt \n')
 		print("Killed at", k, que[-1])
 		sys.stdout.write('\033[0m')	#reset color
-
+		
+		
+		
