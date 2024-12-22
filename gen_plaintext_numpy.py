2ce162e190831ae94ccca51eed50724281aa1cd4
Amir Azmi, CQF <amirmazmi@users.noreply.github.com>
2024-12-22 20:46:56 +0800

    add duckdb

--- gen_plaintext_numpy.py
+++ gen_plaintext_numpy.py
@@ -9,6 +9,8 @@
 #-----------------------------------------------------------------------------------------
 
 import numpy as np
+from time import perf_counter
+
 
 
 charlist = "!#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
@@ -88,16 +90,54 @@ for k in range(2):          # 4-char output [DO NOT GO MORE - size memory explos
 # FINAL
 #-------------------------------------------
 arr_add = arr_char
+
+t_start = perf_counter()
 for k in range(3):          # 4-char output [DO NOT GO MORE - size memory explosion ]
-    print(f'size: {arr_add.shape[0]:,}')
+    print(f'size: {arr_add.shape[0]:,}', end="\t")
     arr_add = np.char.add( np.repeat( arr_char, arr_add.shape[0]),
                            np.tile( arr_add, (arr_char.shape[0]))
                            )
+    t_int = perf_counter()
+    print(f"Time taken for loop {k}: {t_int - t_start:.3f} seconds")
 else:
     print(f'size: {arr_add.shape[0]:,}' +
           f'\n\n{arr_add[7164695:7164699]}' +
           '\n\n  END\n')
 
+t_end = perf_counter()
+print(f"Time taken: {t_end - t_start:.3f} seconds")
 
-arr_add[:30]
 
+arr_add.shape # (71639296,)
+
+#------------------------------
+# loop through the following line one by one to join into 8 char
+
+# lala = np.char.add( np.repeat( np.array(arr_add[0]), arr_add.shape[0]), arr_add)
+
+
+#------------------------------
+# save to db
+
+conn = duckdb.connect("plaintext_word.duckdb")
+
+conn.sql("""
+CREATE TABLE IF NOT EXISTS char4 (
+    phrase VARCHAR PRIMARY KEY
+    )""")
+
+conn.sql("show tables")
+conn.sql("select * from char4")
+
+conn.sql("insert into char4 select * from arr_add")
+conn.sql(f"select phrase from char4 limit 20 offset {51_716_452}")
+
+conn.sql("select count(phrase) from char4")
+
+
+# conn.sql("DROP TABLE IF EXISTS char4")
+
+# wawa = arr_add[6543210:6543310]
+# conn.sql("insert into char4 select * from wawa")
+
+conn.close()
